672c1783aaf0ef3a0841429df7b85043dc95da63
Johannes Schüth <git@jotschi.de>
2023-03-30 10:11:24 +0200

    Fix empty line handling in SnippetReader

--- src/main/java/org/apache/maven/doxia/macro/snippet/SnippetReader.java
+++ src/main/java/org/apache/maven/doxia/macro/snippet/SnippetReader.java
@@ -33,208 +33,200 @@ import org.codehaus.plexus.util.IOUtil;
 /**
  * Utility class for reading snippets.
  */
-public class SnippetReader
-{
-    /** System-dependent EOL. */
-    private static final String EOL = System.getProperty( "line.separator" );
+public class SnippetReader {
+	/** System-dependent EOL. */
+	private static final String EOL = System.getProperty("line.separator");
 
-    /** The source. */
-    private URL source;
+	/** The source. */
+	private URL source;
 
-    /** The encoding of the source. */
-    private String encoding;
+	/** The encoding of the source. */
+	private String encoding;
 
-    /**
-     * Constructor.
-     *
-     * @param src The source
-     * @param encoding The file encoding
-     */
-    public SnippetReader( URL src, String encoding )
-    {
-        this.source = src;
-        this.encoding = encoding;
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @param src
+	 *            The source
+	 * @param encoding
+	 *            The file encoding
+	 */
+	public SnippetReader(URL src, String encoding) {
+		this.source = src;
+		this.encoding = encoding;
+	}
 
-    /**
-     * Constructor.
-     *
-     * @param src The source
-     */
-    public SnippetReader( URL src )
-    {
-        this( src, null ) ;
-    }
+	/**
+	 * Constructor.
+	 *
+	 * @param src
+	 *            The source
+	 */
+	public SnippetReader(URL src) {
+		this(src, null);
+	}
 
-    /**
-     * Reads the snippet with given id.
-     *
-     * @param snippetId The id of the snippet.
-     * @return The snippet.
-     * @throws java.io.IOException if something goes wrong.
-     */
-    public StringBuffer readSnippet( String snippetId )
-        throws IOException
-    {
-        List<String> lines = readLines( snippetId );
-        int minIndent = minIndent( lines );
-        StringBuffer result = new StringBuffer();
-        for ( String line : lines )
-        {
-            result.append( line.substring( minIndent ) );
-            result.append( EOL );
-        }
-        return result;
-    }
+	/**
+	 * Reads the snippet with given id.
+	 *
+	 * @param snippetId
+	 *            The id of the snippet.
+	 * @return The snippet.
+	 * @throws java.io.IOException
+	 *             if something goes wrong.
+	 */
+	public StringBuffer readSnippet(String snippetId)
+		throws IOException {
+		List<String> lines = readLines(snippetId);
+		int minIndent = minIndent(lines);
+		StringBuffer result = new StringBuffer();
+		for (String line : lines) {
+			// PATCH: Fix handling of empty lines
+			if (!line.isBlank()) {
+				result.append(line.substring(minIndent));
+			}
+			result.append(EOL);
+		}
+		return result;
+	}
 
-    /**
-     * Returns the minimal indent of all the lines in the given List.
-     *
-     * @param lines A List of lines.
-     * @return the minimal indent.
-     */
-    int minIndent( List<String> lines )
-    {
-        int minIndent = Integer.MAX_VALUE;
-        for ( String line : lines )
-        {
-            minIndent = Math.min( minIndent, indent( line ) );
-        }
-        return minIndent;
-    }
+	/**
+	 * Returns the minimal indent of all the lines in the given List.
+	 *
+	 * @param lines
+	 *            A List of lines.
+	 * @return the minimal indent.
+	 */
+	int minIndent(List<String> lines) {
+		int minIndent = Integer.MAX_VALUE;
+		for (String line : lines) {
+			// PATCH: Fix handling of empty lines
+			if (line.isBlank()) {
+				continue;
+			}
+			minIndent = Math.min(minIndent, indent(line));
+		}
+		return minIndent;
+	}
 
-    /**
-     * Returns the indent of the given line.
-     *
-     * @param line A line.
-     * @return the indent.
-     */
-    int indent( String line )
-    {
-        char[] chars = line.toCharArray();
-        int indent = 0;
-        for ( ; indent < chars.length; indent++ )
-        {
-        	// PATCH: Fix indentation handling for code with tabs
-            if ( chars[indent] != ' ' && chars[indent] != '\t' )
-            {
-                break;
-            }
-        }
-        return indent;
-    }
+	/**
+	 * Returns the indent of the given line.
+	 *
+	 * @param line
+	 *            A line.
+	 * @return the indent.
+	 */
+	int indent(String line) {
+		char[] chars = line.toCharArray();
+		int indent = 0;
+		for (; indent < chars.length; indent++) {
+			// PATCH: Fix indentation handling for code with tabs
+			if (chars[indent] != ' ' && chars[indent] != '\t') {
+				break;
+			}
+		}
+		return indent;
+	}
 
-    /**
-     * Reads the snippet and returns the lines in a List.
-     *
-     * @param snippetId The id of the snippet.
-     * @return A List of lines.
-     * @throws IOException if something goes wrong.
-     */
-    private List<String> readLines( String snippetId )
-        throws IOException
-    {
-        BufferedReader reader;
-        if ( encoding == null || "".equals( encoding ) )
-        {
-            reader = new BufferedReader( new InputStreamReader( source.openStream() ) );
-        }
-        else
-        {
-            reader = new BufferedReader( new InputStreamReader( source.openStream(), encoding ) );
-        }
+	/**
+	 * Reads the snippet and returns the lines in a List.
+	 *
+	 * @param snippetId
+	 *            The id of the snippet.
+	 * @return A List of lines.
+	 * @throws IOException
+	 *             if something goes wrong.
+	 */
+	private List<String> readLines(String snippetId)
+		throws IOException {
+		BufferedReader reader;
+		if (encoding == null || "".equals(encoding)) {
+			reader = new BufferedReader(new InputStreamReader(source.openStream()));
+		} else {
+			reader = new BufferedReader(new InputStreamReader(source.openStream(), encoding));
+		}
 
-        List<String> lines = new ArrayList<>();
-        try
-        {
-            boolean capture = false;
-            String line;
-            boolean foundStart = false;
-            boolean foundEnd = false;
-            boolean hasSnippetId = StringUtils.isNotEmpty( snippetId );
-            while ( ( line = reader.readLine() ) != null )
-            {
-                if ( !hasSnippetId )
-                {
-                    lines.add( line );
-                }
-                else
-                {
-                    if ( isStart( snippetId, line ) )
-                    {
-                        capture = true;
-                        foundStart = true;
-                    }
-                    else if ( isEnd( snippetId, line ) )
-                    {
-                        foundEnd = true;
-                        break;
-                    }
-                    else if ( capture )
-                    {
-                        lines.add( line );
-                    }
-                }
-            }
+		List<String> lines = new ArrayList<>();
+		try {
+			boolean capture = false;
+			String line;
+			boolean foundStart = false;
+			boolean foundEnd = false;
+			boolean hasSnippetId = StringUtils.isNotEmpty(snippetId);
+			while ((line = reader.readLine()) != null) {
+				if (!hasSnippetId) {
+					lines.add(line);
+				} else {
+					if (isStart(snippetId, line)) {
+						capture = true;
+						foundStart = true;
+					} else if (isEnd(snippetId, line)) {
+						foundEnd = true;
+						break;
+					} else if (capture) {
+						lines.add(line);
+					}
+				}
+			}
 
-            if ( hasSnippetId && !foundStart )
-            {
-                throw new IOException( "Failed to find START of snippet " + snippetId + " in file at URL: " + source );
-            }
-            if ( hasSnippetId && !foundEnd )
-            {
-                throw new IOException( "Failed to find END of snippet " + snippetId + " in file at URL: " + source );
-            }
-        }
-        finally
-        {
-            IOUtil.close( reader );
-        }
-        return lines;
-    }
+			if (hasSnippetId && !foundStart) {
+				throw new IOException("Failed to find START of snippet " + snippetId + " in file at URL: " + source);
+			}
+			if (hasSnippetId && !foundEnd) {
+				throw new IOException("Failed to find END of snippet " + snippetId + " in file at URL: " + source);
+			}
+		} finally {
+			IOUtil.close(reader);
+		}
+		return lines;
+	}
 
-    /**
-     * Determines if the given line is a start demarcator.
-     *
-     * @param snippetId the id of the snippet.
-     * @param line the line.
-     * @return True, if the line is a start demarcator.
-     */
-    protected boolean isStart( String snippetId, String line )
-    {
-        return isDemarcator( snippetId, "START", line );
-    }
+	/**
+	 * Determines if the given line is a start demarcator.
+	 *
+	 * @param snippetId
+	 *            the id of the snippet.
+	 * @param line
+	 *            the line.
+	 * @return True, if the line is a start demarcator.
+	 */
+	protected boolean isStart(String snippetId, String line) {
+		return isDemarcator(snippetId, "START", line);
+	}
 
-    /**
-     * Determines if the given line is a demarcator.
-     *
-     * @param snippetId the id of the snippet.
-     * @param what Identifier for the demarcator.
-     * @param line the line.
-     * @return True, if the line is a start demarcator.
-     */
-    protected static boolean isDemarcator( String snippetId, String what, String line )
-    {
-        // SNIPPET and what are case insensitive
-        // SNIPPET and what can switch order
-        String snippetRegExp = "(^|\\W)(?i:SNIPPET)($|\\W)";
-        String snippetIdRegExp = "(^|\\W)" + snippetId + "($|\\W)";
-        String whatRegExp = "(^|\\W)(?i:" + what + ")($|\\W)";
-        
-        return Pattern.compile( snippetRegExp ).matcher( line ).find()
-            && Pattern.compile( whatRegExp ).matcher( line ).find()
-            && Pattern.compile( snippetIdRegExp ).matcher( line ).find();
-    }
+	/**
+	 * Determines if the given line is a demarcator.
+	 *
+	 * @param snippetId
+	 *            the id of the snippet.
+	 * @param what
+	 *            Identifier for the demarcator.
+	 * @param line
+	 *            the line.
+	 * @return True, if the line is a start demarcator.
+	 */
+	protected static boolean isDemarcator(String snippetId, String what, String line) {
+		// SNIPPET and what are case insensitive
+		// SNIPPET and what can switch order
+		String snippetRegExp = "(^|\\W)(?i:SNIPPET)($|\\W)";
+		String snippetIdRegExp = "(^|\\W)" + snippetId + "($|\\W)";
+		String whatRegExp = "(^|\\W)(?i:" + what + ")($|\\W)";
 
-    /**
-     * Determines if the given line is an end demarcator.
-     *
-     * @param snippetId the id of the snippet.
-     * @param line the line.
-     * @return True, if the line is an end demarcator.
-     */
-    protected boolean isEnd( String snippetId, String line )
-    {
-        return isDemarcator( snippetId, "END", line );
-    }
+		return Pattern.compile(snippetRegExp).matcher(line).find()
+			&& Pattern.compile(whatRegExp).matcher(line).find()
+			&& Pattern.compile(snippetIdRegExp).matcher(line).find();
+	}
+
+	/**
+	 * Determines if the given line is an end demarcator.
+	 *
+	 * @param snippetId
+	 *            the id of the snippet.
+	 * @param line
+	 *            the line.
+	 * @return True, if the line is an end demarcator.
+	 */
+	protected boolean isEnd(String snippetId, String line) {
+		return isDemarcator(snippetId, "END", line);
+	}
 }
